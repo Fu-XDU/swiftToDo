20d207fc27b781607569a8e3ef8798331f5663db
付铭 <fuming.137@icloud.com>
2022-07-13 18:08:54 +0800

    update

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -13,28 +13,57 @@ struct Item: Identifiable {
 }
 
 var Items = [
-    Item(title: "Id:\(1)"),
-    Item(title: "Id:\(1)"),
-    Item(title: "Id:\(1)"),
-    Item(title: "Id:\(1)"),
-    Item(title: "Id:\(1)"),
-    Item(title: "Id:\(1)"),
-    Item(title: "Id:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
+    Item(title: "ID:\(1)"),
 ]
 
 struct ContentView: View {
     @State var dataItems = Items;
+
+    init() {
+        UITableView.appearance().backgroundColor = .black
+        UITableView.appearance().sectionFooterHeight = 1
+        UITableView.appearance().sectionHeaderHeight = 1
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(red: 0.44, green: 0.55, blue: 0.89, alpha: 1)]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(red: 0.44, green: 0.55, blue: 0.89, alpha: 1)]
+    }
+
     var body: some View {
         NavigationView {
             List {
                 ForEach(dataItems) { element in
-                    Button(element.title) {
-
+                    Section {
+                        HStack {
+                            Circle()
+                                    .stroke(Color(red: 0.58, green: 0.58, blue: 0.58), style: StrokeStyle(lineWidth: 2))
+                                    .frame(width: 22, height: 22).padding(EdgeInsets(top: 0, leading: -5, bottom: 0, trailing: 10))
+                            Text("Hello World. \(element.title)")
+                                    .foregroundColor(.white)
+                            Spacer()
+                            Image(systemName: "star")
+                                    .frame(maxWidth: 20, maxHeight: .infinity, alignment: .trailing)
+                                    .foregroundColor(Color(red: 0.58, green: 0.58, blue: 0.58))
+                                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: -10))
+                        }
                     }
-                }.onDelete(perform: deleteRow)
-
-            }.background(Color.orange)
-                    .padding().navigationTitle("计划").background(Color.orange)
+                }
+                        .onDelete(perform: deleteRow)
+                        .listRowBackground(Color(red: 0.13, green: 0.13, blue: 0.13))
+                        .frame(height: 40)
+            }
+                    .listStyle(InsetGroupedListStyle())
+                    .navigationTitle(Text("计划"))
         }
     }
 
